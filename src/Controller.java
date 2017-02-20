5a42a4ec339f4608731dc19096d66f4245b3c978
bigstepdenmark <i@bigstep.dk>
2017-02-20 22:04:15 +0100

    Excercise EQUIVALENCE PARTITIONING, BOUNDARY VALUE ANALYSIS done

--- src/Controller.java
+++ src/Controller.java
@@ -3,19 +3,16 @@ import java.time.YearMonth;
 
 class Controller
 {
-    // Ex. EQUIVALENCE PARTITIONING 1..
     boolean isEven(int n)
     {
         return n > 0 && n <= 1000 && n % 2 == 0;
     }
 
-    // Ex. EQUIVALENCE PARTITIONING 2.
     boolean checkSalary(int amount)
     {
         return amount >= 1000 && amount <= 75000;
     }
 
-    // Ex. EQUIVALENCE PARTITIONING 3.
     static int getNumDaysinMonth(int month, int year)
     {
         try
